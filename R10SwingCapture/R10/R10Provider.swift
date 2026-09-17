2918e674b6cd55b10af941ce59d99edd89fee2d4
de4dm0use <whetton.oscar@gmail.com>
2026-09-17 18:00:30 +0100

    Add R10 provider protocol

---
+++ R10SwingCapture/R10/R10Provider.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+protocol R10Provider: AnyObject {
+    var name: String { get }
+    var isConnected: Bool { get }
+    func start() async
+    func stop() async
+    func shots() -> AsyncStream<AppShot>
+}
