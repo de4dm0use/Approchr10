3f73ad264a5a430ce37b5688962f9b7e28963c9d
de4dm0use <whetton.oscar@gmail.com>
2026-09-17 18:22:55 +0100

    Fix StateObject initialization for non-throwing AppModel

--- R10SwingCapture/R10SwingCaptureApp.swift
+++ R10SwingCapture/R10SwingCaptureApp.swift
@@ -6,11 +6,7 @@ struct R10SwingCaptureApp: App {
     @StateObject private var app: AppModel
 
     init() {
-        do {
-            _app = StateObject(wrappedValue: try AppModel())
-        } catch {
-            fatalError("Failed to create app model: \(error)")
-        }
+        _app = StateObject(wrappedValue: AppModel())
     }
 
     var body: some Scene {
